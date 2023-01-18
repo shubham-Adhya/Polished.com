2726ab1a5c019b90dc91e22ab34d0e300c4d13c7
shubham-Adhya <119453418+shubham-Adhya@users.noreply.github.com>
2023-01-18 16:21:04 +0530

    nav1 created

---
+++ scripts/common-script.js
@@ -0,0 +1,14 @@
+let loader=document.getElementById("preloader");
+    window.addEventListener("load",()=>{
+        setTimeout(()=>{
+            loader.style.display = "none";
+        },1470)
+    })
+let dancingText=document.querySelector("#dancingText")
+    setInterval(()=>{
+        dancingText.style.opacity="0"
+        setTimeout(()=>{
+            dancingText.style.opacity="1"
+        },1000)
+    },3000)
+    
