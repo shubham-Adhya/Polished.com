55a96b180980320b595d01cce28a807c33312b9e
shubham-Adhya <119453418+shubham-Adhya@users.noreply.github.com>
2023-01-20 00:04:32 +0530

    end of day3

---
+++ scripts/dashboard.js
@@ -0,0 +1,12 @@
+function hrefFunction1(){
+    window.location.href="./dashboard.html"
+}
+function hrefFunction2(){
+    window.location.href="./cart.html"
+}
+function hrefFunction3(){
+    window.location.href="./orders.html"
+}
+function hrefFunction4(){
+    window.location.href="./index.html"
+}
