18459b1473902502f5921c6d075b7effef0c5d83
shubham-Adhya <119453418+shubham-Adhya@users.noreply.github.com>
2023-01-19 13:22:34 +0530

    footers created

--- scripts/common-script.js
+++ scripts/common-script.js
@@ -11,4 +11,24 @@ let dancingText=document.querySelector("#dancingText")
             dancingText.style.opacity="1"
         },1000)
     },3000)
+
+
+document.addEventListener("click", (e)=>{
+    const isDropDownButton=e.target.matches("[data-dropdown-button]")
+    if(!isDropDownButton && e.target.closest(`[data-dropdown]`) != null) return
+
+    let currentDropDown
+    if (isDropDownButton){
+        currentDropDown=e.target.closest(`[data-dropdown]`)
+        currentDropDown.classList.toggle('active')
+    }
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown=>{
+        if(dropdown===currentDropDown) return
+        dropdown.classList.remove('active')
+    })
+})
+
+
+
+
     
